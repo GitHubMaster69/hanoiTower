5b1d6a511a3e3855686eb05231f38195d4029574
Biggie Cheese <50361292+GitHubMaster69@users.noreply.github.com>
2019-10-04 14:47:14 +0200

    Initial commit

---
+++ src/mainyionkz/Main.java
@@ -0,0 +1,40 @@
+package mainyionkz;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public void setup(){
+        size(100,100);
+    }
+
+    public void draw(){
+        println("Gay");
+    }
+
+
+
+    public void Rekursiv() {
+
+
+    }
+
+
+    public static void main(String[] args){
+        Main.main("mainyionkz.Main");
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
---
+++ src/mainyionkz/Tower.java
@@ -0,0 +1,4 @@
+package mainyionkz;
+
+public class Tower {
+}
