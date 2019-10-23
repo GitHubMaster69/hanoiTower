c64419371bd6d3b63326d6c82a08fca5a0b4c1e1
Tuxtampa <37031351+Tuxtampa@users.noreply.github.com>
2019-10-23 08:57:42 +0200

    Bit of class stuff

--- src/mainyionkz/Blockz.java
+++ src/mainyionkz/Blockz.java
@@ -1,4 +1,9 @@
 package mainyionkz;
 
 public class Blockz {
+    private int blockNumber;
+
+    Blockz(int num){
+        blockNumber = num;
+    }
 }
--- src/mainyionkz/Tower.java
+++ src/mainyionkz/Tower.java
@@ -1,4 +1,12 @@
 package mainyionkz;
 
+import java.util.ArrayList;
+
 public class Tower {
+    public int towerNumber;
+
+    Tower(int num){
+        towerNumber = num;
+        new ArrayList(8);
+    }
 }
