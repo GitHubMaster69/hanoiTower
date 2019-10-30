f4f3427b1fb59b450899de1127704b5a44502dbc
Biggie Cheese <50361292+GitHubMaster69@users.noreply.github.com>
2019-10-30 14:00:02 +0100

    Rekursiv math thing

---
+++ src/mainyionkz/Rekursiv.java
@@ -0,0 +1,23 @@
+package mainyionkz;
+
+public class Rekursiv {
+
+        static void towerOfHanoi(int n, char from_rod, char to_rod, char mid_rod)
+        {
+            if (n == 1)
+            {
+                System.out.println("Move disk 1 from rod " +  from_rod + " to rod " + to_rod);
+                return;
+            }
+            towerOfHanoi(n-1, from_rod, mid_rod, to_rod);
+            System.out.println("Move disk " + n + " from rod " +  from_rod + " to rod " + to_rod);
+            towerOfHanoi(n-1, mid_rod, to_rod, from_rod);
+        }
+
+        public static void beb()
+        {
+            int n = 4;
+            towerOfHanoi(n, 'A', 'C', 'B');
+        }
+    }
+
